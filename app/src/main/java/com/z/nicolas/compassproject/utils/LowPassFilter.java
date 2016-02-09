90b983668e2e799ab60a0c96673f1a4cff5ee51d
Nicolas-Z <mzylkowski@gmail.com>
2016-02-09 22:14:11 +0100

    Fix for low pass filter - use proper equation and set higher threshold

--- app/src/main/java/com/z/nicolas/compassproject/utils/LowPassFilter.java
+++ app/src/main/java/com/z/nicolas/compassproject/utils/LowPassFilter.java
@@ -2,7 +2,7 @@ package com.z.nicolas.compassproject.utils;
 
 public class LowPassFilter {
 
-    public static final float ALPHA = 0.2f;
+    public static final float ALPHA = 0.97f;
 
     public static float[] filter(float[] input, float[] output) {
         if (null == output) {
@@ -10,7 +10,7 @@ public class LowPassFilter {
         }
 
         for (int i = 0; i < input.length; i++) {
-            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+            output[i] = ALPHA * output[i] + (1f - ALPHA) * input[i];
         }
 
         return output;
